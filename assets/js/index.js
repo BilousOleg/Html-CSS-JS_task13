878d76c98072f5b27289077d2190aa68e7e5d299
BilousOleg <olezhka82802@gmail.com>
2026-01-18 22:38:16 +0200

    Add task solution

---
+++ assets/js/index.js
@@ -0,0 +1,185 @@
+const products = [
+  {
+    id: 1,
+    name: 'Молоко',
+    price: 25,
+    quantity: 14,
+    category: 'Молочні продукти',
+  },
+  {
+    id: 2,
+    name: 'Хліб',
+    price: 12,
+    quantity: 100,
+    category: 'Хлібобулочні вироби',
+  },
+  { id: 3, name: 'Яйця', price: 20, quantity: 12, category: 'Птахоферма' },
+  {
+    id: 4,
+    name: 'Масло',
+    price: 35,
+    quantity: 31,
+    category: 'Молочні продукти',
+  },
+  {
+    id: 5,
+    name: 'Цукор',
+    price: 15,
+    quantity: 44,
+    category: 'Кондитерські вироби',
+  },
+  {
+    id: 6,
+    name: 'Картопля',
+    price: 10,
+    quantity: 50,
+    category: 'Овочі та фрукти',
+  },
+  {
+    id: 7,
+    name: 'Помідори',
+    price: 30,
+    quantity: 71,
+    category: 'Овочі та фрукти',
+  },
+  {
+    id: 8,
+    name: 'Індичка',
+    price: 40,
+    quantity: 10,
+    category: "М'ясо та м'ясні вироби",
+  },
+  {
+    id: 9,
+    name: 'Яловичина',
+    price: 25,
+    quantity: 120,
+    category: "М'ясо та м'ясні вироби",
+  },
+  {
+    id: 10,
+    name: 'Макарони',
+    price: 18,
+    quantity: 140,
+    category: 'Макаронні вироби',
+  },
+  {
+    id: 11,
+    name: 'Сир',
+    price: 50,
+    quantity: 19,
+    category: 'Молочні продукти',
+  },
+  {
+    id: 12,
+    name: 'Печиво',
+    price: 22,
+    quantity: 56,
+    category: 'Кондитерські вироби',
+  },
+  {
+    id: 13,
+    name: 'Куряче філе',
+    price: 40,
+    quantity: 12,
+    category: "М'ясо та м'ясні вироби",
+  },
+  {
+    id: 14,
+    name: 'Банани',
+    price: 18,
+    quantity: 63,
+    category: 'Овочі та фрукти',
+  },
+  {
+    id: 15,
+    name: 'Свинина',
+    price: 60,
+    quantity: 3,
+    category: "М'ясо та м'ясні вироби",
+  },
+  {
+    id: 16,
+    name: 'Чай чорний',
+    price: 30,
+    quantity: 5,
+    category: 'Чай та кава',
+  },
+  { id: 17, name: 'Кава', price: 50, quantity: 8, category: 'Чай та кава' },
+  {
+    id: 18,
+    name: 'Сода',
+    price: 8,
+    quantity: 2,
+    category: 'Безалкогольні напої',
+  },
+  {
+    id: 19,
+    name: 'Сметана',
+    price: 25,
+    quantity: 18,
+    category: 'Молочні продукти',
+  },
+  {
+    id: 20,
+    name: 'Яблука',
+    price: 20,
+    quantity: 88,
+    category: 'Овочі та фрукти',
+  },
+];
+
+// Завдання 1
+
+// Можна було б і одним методом reduce() з умовою в середині
+console.log(
+  products
+    .filter((p) => p.category === 'Овочі та фрукти')
+    .reduce((accum, p) => (accum += p.quantity), 0),
+);
+
+// Завдання 2
+
+console.log(
+  products
+    .filter((p) => p.category === 'Молочні продукти')
+    .reduce((accum, p) => (accum += p.quantity * p.price), 0),
+);
+
+// Завдання 3
+
+console.log(
+  [...products].sort((p1, p2) => (p2.quantity > p1.quantity ? 1 : -1)),
+);
+
+// Завдання 4
+// Спрощений варіант, оскільки продуктів з однаковою ціною могло б бути декілька,
+// і довелося б робити масив об'єктів з однаковою ціною в такому випадку
+
+const mostExpensive = products.reduce((accum, p) => {
+  if (p.price > accum.price) {
+    accum = { ...p };
+  }
+  return accum;
+});
+
+const leastExpensive = products.reduce((accum, p) => {
+  if (p.price < accum.price) {
+    accum = { ...p };
+  }
+  return accum;
+});
+
+console.log('Найдорожчий продукт:', mostExpensive.name, mostExpensive.price);
+console.log('Найдешевший продукт:', leastExpensive.name, leastExpensive.price);
+
+// Завдання 5
+
+const meatProducts = products.filter(
+  (p) => p.category === "М'ясо та м'ясні вироби",
+);
+
+console.log(
+  meatProducts.reduce((accum, p) => (accum += p.price), 0) /
+    meatProducts.length,
+);
